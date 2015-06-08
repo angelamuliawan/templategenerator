540971f0bb55d3224c905b4243209a310b6331f8
Angela Muliawan <amuliawan93@gmail.com>
2015-06-08 22:14:05 +0700

    forgot password

--- engine/js/home.js
+++ engine/js/home.js
@@ -4,9 +4,23 @@ $(document).ready(function(){
 			$(".registerForm").fadeIn();
 		});
 	})
-	$("#btnBackToLogin").click(function(){
+	$("#btnBackToLoginReg").click(function(){
 		$(".registerForm").fadeOut(function(){
 			$(".loginForm").fadeIn();
+		});
+	})
+	$("#btnForgotPassword").click(function(){
+		$(".loginForm").fadeOut(function(){
+			$(".forgotPasswordForm").fadeIn();
+			$("#modalLabel").text("Forgot Password");
+			$("#modalLabelContent").text("We will send confirmation and new password to your email.");
+		});
+	})
+	$("#btnBackToLoginFor").click(function(){
+		$(".forgotPasswordForm").fadeOut(function(){
+			$(".loginForm").fadeIn();
+			$("#modalLabel").text("Login | Register");
+			$("#modalLabelContent").text("Input email and password to start managing your website.");
 		});
 	})
 });
