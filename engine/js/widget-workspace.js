9554049dc04141832f4bc979f3ff4a4fd56df80a
silvranz <silvranz@gmail.com>
2015-06-27 21:13:14 +0700

    redesign fix resize

--- engine/js/widget-workspace.js
+++ engine/js/widget-workspace.js
@@ -1,4 +1,5 @@
 /* Properties - global variable*/
+var accrossPoint = {};
 var cursorMode = 0;
 dragElement = null;
 // 0=numerik, 1=color, 2=ddl(option), 3=other(text)
@@ -75,15 +76,11 @@ var WIDGETELEMENTCLASS = "widgetElement";
 
 /* End Properties */
 /* Method - global function */
-var changeColorPickerHandler = 
-		function(e){
-			console.log()
-		}
 var changePropertiesHandler = 
 		function(e){
 			$(this).addClass("syncVal");
-			var popupTop = $(this).offset().top;
-			var popupLeft = $("#propertiesSection").offset().left + $("#propertiesSection").width();
+			var popupTop = $(this).offset().top + $(this).height();
+			var popupLeft = $(this).offset().left-30;
 			var propertiesIndex = $(this).attr("data-index");
 			var divPopup = $("#changeValTemplate"+listProperties[propertiesIndex].valueType).clone().attr("id","popupValue");
 			$(divPopup).offset({
@@ -134,6 +131,7 @@ var changedPropVal =
 var dragElementStart = 
 		function(e){
 			e.preventDefault();
+			e.target = $(e.target).parent()[0];
 			dragElement = e.target;
 			$(e.target).addClass("draggedElement");
 			normalOffsetLeft = $(e.target).offset().left;
@@ -169,46 +167,70 @@ var dragMove =
 					break;
 					case 1:
 					//kiri-bawah kanan-atas
+					var newWidth = e.pageX - accrossPoint.x;
+					var newHeight = e.pageY - accrossPoint.y;
+					if(startDragWidgetLeft > normalWidth/2){
+						//kanan-bawah
+						$(dragElement).width(Math.sqrt(Math.pow(newWidth,2)));
+						$(dragElement).height(Math.sqrt(Math.pow(newHeight,2)));
+					}
+					else{
+						//kiri-atas
+						$(dragElement).offset({
+							left:e.pageX,
+							top:e.pageY
+						})
+						$(dragElement).width(Math.sqrt(Math.pow(newWidth,2)));
+						$(dragElement).height(Math.sqrt(Math.pow(newHeight,2)));
+					}
 					break;
 					case 2:
 					//kiri-atas kanan-bawah
-					var divX = e.pageX - startDragWidgetLeft - normalOffsetLeft;
-					var divY = e.pageY - startDragWidgetTop - normalOffsetTop;
-					$(dragElement).offset({
-						left:normalOffsetLeft+divX,
-						top:normalOffsetTop+divY
-					})
-					$(dragElement).width(normalWidth+Math.sqrt(Math.pow(divX,2)));
-					$(dragElement).height(normalHeight+Math.sqrt(Math.pow(divY,2)));
+					var newWidth = e.pageX - accrossPoint.x;
+					var newHeight = e.pageY - accrossPoint.y;
+					if(startDragWidgetLeft > normalWidth/2){
+						//kanan-bawah
+						$(dragElement).width(Math.sqrt(Math.pow(newWidth,2)));
+						$(dragElement).height(Math.sqrt(Math.pow(newHeight,2)));
+					}
+					else{
+						//kiri-atas
+						$(dragElement).offset({
+							left:e.pageX,
+							top:e.pageY
+						})
+						$(dragElement).width(Math.sqrt(Math.pow(newWidth,2)));
+						$(dragElement).height(Math.sqrt(Math.pow(newHeight,2)));
+					}
 					break;
 					case 3:
 					//kiri kanan
-					var divX = e.pageX - startDragWidgetLeft - normalOffsetLeft;
+					var newWidth = e.pageX - accrossPoint.x;
 					if(startDragWidgetLeft > normalWidth/2){
 						//kanan
-						$(dragElement).width(normalWidth+Math.sqrt(Math.pow(divX,2)));
+						$(dragElement).width(Math.sqrt(Math.pow(newWidth,2)));
 					}
 					else{
 						//kiri
 						$(dragElement).offset({
-							left:normalOffsetLeft+divX
+							left:e.pageX
 						})
-						$(dragElement).width(normalWidth+Math.sqrt(Math.pow(divX,2)));
+						$(dragElement).width(Math.sqrt(Math.pow(newWidth,2)));
 					}
 					break;
 					case 4:
 					//atas bawah
-					var divY = e.pageY - startDragWidgetTop - normalOffsetTop;
+					var newHeight = e.pageY - accrossPoint.y;
 					if(startDragWidgetTop >normalHeight/2){
 						//bawah
-						$(dragElement).height(normalHeight+Math.sqrt(Math.pow(divY,2)));
+						$(dragElement).height(Math.sqrt(Math.pow(newHeight,2)));
 					}
 					else{
 						//atas
 						$(dragElement).offset({
-							top:normalOffsetTop+divY
+							top:e.pageY
 						})
-						$(dragElement).height(normalHeight+Math.sqrt(Math.pow(divY,2)));
+						$(dragElement).height(Math.sqrt(Math.pow(newHeight,2)));
 					}
 					break;
 				}							
@@ -216,22 +238,36 @@ var dragMove =
 		}
 var dropHandler =
 		function(e){
-			var tempElement = $(".dragItem").clone().addClass("widgetElement").removeAttr("draggable").css({
+			var tempElement = $(".dragItem").next().find(".cloneElement").clone().css({
+									'width':'100%',
+									"height":"100%"
+								});
+			var container = $("#widgetContainer").clone().removeAttr("id");
+			$(container).css({
+				position:"absolute",
+				top:e.originalEvent.clientY+document.body.scrollTop-$(this).offset().top-startPointerTop,
+				left:e.originalEvent.clientX+document.body.scrollLeft-$(this).offset().left-startPointerLeft,
+			})
+			$(container).append(tempElement);
+			$(this).append(container);
+			var widgetCover = $("<div>").addClass("widgetElement").css({
+									"position":"absolute",
 									"cursor":"move",
 									"-webkit-touch-callout": "none",
 									"-webkit-user-select": "none",
 									"-khtml-user-select": "none",
 									"-moz-user-select": "none",
 									"-ms-user-select": "none",
-									"user-select": "none"
+									"user-select": "none",
+									"height":"100%",
+									"width":"100%"
 								});
-			$(tempElement).css({
-				position:"absolute",
-				top:e.originalEvent.clientY+document.body.scrollTop-$(this).offset().top-startPointerTop,
-				left:e.originalEvent.clientX+document.body.scrollLeft-$(this).offset().left-startPointerLeft,
+			$(widgetCover).click(editProp).on("mousedown",dragElementStart).mousemove(widgetElementHover).hover(function(e){
+				$(this).css("border","1px dashed black");
+			},function(e){
+				$(this).css("border","none");
 			});
-			$(tempElement).click(editProp).on("mousedown",dragElementStart).mousemove(widgetElementHover);
-			$(this).append(tempElement);
+			$(container).prepend(widgetCover);
 			$(".dragItem").removeClass("dragItem");
 			e.preventDefault();
 		};
@@ -246,8 +282,8 @@ var eachListEvent =
 var editProp = 
 		function(e){
 			e.preventDefault();
-			selectedElement = this;
-			loadProperties(this);
+			selectedElement = $(this).next();
+			loadProperties(selectedElement);
 			$(".editValue").click(changePropertiesHandler);
 		};
 var globalMouseUp = 
@@ -271,7 +307,6 @@ var loadProperties =
 			$("#classValue").val($(object).attr("class").replace(WIDGETELEMENTCLASS," "));
 			$("#idValue").val($(object).attr("id"));
 			for(var i=0;i<listProperties.length;i++){
-				console.log(listProperties[i]);
 				$("#"+listProperties[i].name+"Value").text($(object).css(listProperties[i].name));
 			}			
 		};
@@ -284,34 +319,71 @@ var normalWidgetDragStartHandler =
 		}
 var widgetElementHover =
 		function(e){
+			e.target = $(e.target).parent()[0];
 			if($(".draggedElement").length>0)return;
 			if((e.pageX <= $(e.target).offset().left+5&&e.pageY >= $(e.target).height() + $(e.target).offset().top - 5)
 				||
 				(e.pageX >= $(e.target).width() + $(e.target).offset().left - 5 && e.pageY <= $(e.target).offset().top+5)){
 				//kiri-bawah kanan-atas
-				$(e.target).css("cursor","ne-resize");
+				$(e.target).find(".widgetElement").css("cursor","ne-resize");
 				cursorMode = 1;
+				if(e.pageX <= $(e.target).offset().left+5&&e.pageY >= $(e.target).height() + $(e.target).offset().top - 5){
+					//kiri-bawah
+					accrossPoint.x = $(e.target).width() + $(e.target).offset().left;
+					accrossPoint.y = $(e.target).offset().top;
+				}
+				else if(e.pageX >= $(e.target).width() + $(e.target).offset().left - 5 && e.pageY <= $(e.target).offset().top+5){
+					//kanan-atas
+					accrossPoint.x = $(e.target).offset().left;
+					accrossPoint.y = $(e.target).height() + $(e.target).offset().top;
+				}
 			}
 			else if((e.pageX <= $(e.target).offset().left+5&&e.pageY <= $(e.target).offset().top+5)
 					||
 					(e.pageX >= $(e.target).width() + $(e.target).offset().left - 5 &&e.pageY >= $(e.target).height() + $(e.target).offset().top - 5)){
 				//kiri-atas kanan-bawah
-				$(e.target).css("cursor","nw-resize");
+				$(e.target).find(".widgetElement").css("cursor","nw-resize");
 				cursorMode = 2;
+				if(e.pageX <= $(e.target).offset().left+5&&e.pageY <= $(e.target).offset().top+5){
+					//kiri-atas
+					accrossPoint.x = $(e.target).width() + $(e.target).offset().left;
+					accrossPoint.y = $(e.target).height() + $(e.target).offset().top;
+				}
+				else if(e.pageX >= $(e.target).width() + $(e.target).offset().left - 5 &&e.pageY >= $(e.target).height() + $(e.target).offset().top - 5){
+					//kanan-bawah
+					accrossPoint.x = $(e.target).offset().left;
+					accrossPoint.y = $(e.target).offset().top;
+				}
 			}
 			else if(e.pageX <= $(e.target).offset().left+5||e.pageX >= $(e.target).width() + $(e.target).offset().left - 5){
 				//kiri kanan
-				$(e.target).css("cursor","e-resize");
+				$(e.target).find(".widgetElement").css("cursor","e-resize");
 				cursorMode = 3;
+				if(e.pageX <= $(e.target).offset().left+5){
+					//kiri
+					accrossPoint.x = $(e.target).width() + $(e.target).offset().left;
+				}
+				else if(e.pageX >= $(e.target).width() + $(e.target).offset().left - 5){
+					//kanan
+					accrossPoint.x = $(e.target).offset().left;
+				}
 			}
 			else if(e.pageY <= $(e.target).offset().top+5||e.pageY >= $(e.target).height() + $(e.target).offset().top - 5){
 				//atas bawah
-				$(e.target).css("cursor","n-resize");
+				$(e.target).find(".widgetElement").css("cursor","n-resize");
 				cursorMode = 4;
+				if(e.pageY <= $(e.target).offset().top+5){
+					//atas
+					accrossPoint.y = $(e.target).height() + $(e.target).offset().top;
+				}
+				else if(e.pageY >= $(e.target).height() + $(e.target).offset().top - 5){
+					//bawah
+					accrossPoint.y = $(e.target).offset().top;
+				}
 			}
 			else{
 				//tengah
-				$(e.target).css("cursor","move");
+				$(e.target).find(".widgetElement").css("cursor","move");
 				cursorMode = 0;
 			}
 		}
@@ -323,8 +395,8 @@ $(document).ready(function(e){
 		});
 		$(window).on("mouseup",globalMouseUp);
 		$("#editSection").on("drop",dropHandler).mousemove(dragMove);
-		$("#listWidget *").attr("draggable",true);
-		$("#listWidget *").on("dragstart",normalWidgetDragStartHandler);
+		$(".dragBlocker").attr("draggable",true);
+		$(".dragBlocker").on("dragstart",normalWidgetDragStartHandler);
 		$("#editSection *").click(editProp);
 		$(document).mousedown(globalMouseDown);
 		$(listProperties).each(eachListProperties);
